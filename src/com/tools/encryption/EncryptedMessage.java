c34deedb6515bdb5ac8d5c071f48d8fbd15a74c9
Kyle Watson <kwatson7@gmail.com>
2013-11-30 16:25:18 -0800

    Allow the sender of EncryptedMessage to decrypt message

--- src/com/tools/encryption/EncryptedMessage.java
+++ src/com/tools/encryption/EncryptedMessage.java
@@ -10,6 +10,7 @@ public class EncryptedMessage{
 	public byte[] encryptedMessage;			// message was encrypted withs secret key
 	public byte[] signedMessage; 			// the encrypted message was signed with senders private key
 	public byte[] encryptedSecretKey; 		// the secret key is encrypted with receivers public key
+	public byte[] encryptedSecretKeyForSender; // the secret key is ecnrypted with the senders public key too, so he can read later
 	
 	/**
 	 * Create an encrypted message
@@ -37,8 +38,11 @@ public class EncryptedMessage{
 		this.encryptedSecretKey = receiver.encryptWithPublic(nonEncryptedSecretKey);
 		
 		// sign the message
-		PublicPrivateEncryptor sender = new PublicPrivateEncryptor(sendersPrivateKey, null);
+		PublicPrivateEncryptor sender = new PublicPrivateEncryptor(sendersPrivateKey, sendersPublicKey);
 		this.signedMessage = sender.signData(encryptedMessage);
+
+		// encrypt the secret key with the senders public key
+		this.encryptedSecretKeyForSender = sender.encryptWithPublic(nonEncryptedSecretKey);
 	}
 	
 	/**
@@ -48,13 +52,21 @@ public class EncryptedMessage{
 	 * @param encryptedMessage message was encrypted withs secret key
 	 * @param signedMessage the encrypted message was signed with senders private key
 	 * @param encryptedSecretKey the secret key is encrypted with receivers public key
+	 * @param encryptedSecretKeyForSender the secret key is encrypted using the senders public key too.. so he can read it later
 	 */
-	public EncryptedMessage(PublicKey sendersPublicKey, PublicKey receiversPublicKey, byte[] encryptedMessage, byte[] signedMessage, byte[] encryptedSecretKey){
+	public EncryptedMessage(
+			PublicKey sendersPublicKey,
+			PublicKey receiversPublicKey,
+			byte[] encryptedMessage,
+			byte[] signedMessage,
+			byte[] encryptedSecretKey,
+			byte[] 	encryptedSecretKeyForSender){	
 		this.sendersPublicKey = sendersPublicKey;
 		this.receiversPublicKey = receiversPublicKey;
 		this.encryptedMessage = encryptedMessage;
 		this.signedMessage = signedMessage;
 		this.encryptedSecretKey = encryptedSecretKey;
+		this.encryptedSecretKeyForSender= encryptedSecretKeyForSender; 
 	}
 	
 	/**
@@ -75,7 +87,7 @@ public class EncryptedMessage{
 	 * @throws IncorrectPasswordException 
 	 * @throws IOException 
 	 */
-	public String decryptMessage(PrivateKey receiversPrivateKey)
+	public String decryptMessageForReceiver(PrivateKey receiversPrivateKey)
 			throws EncryptionException, IOException, IncorrectPasswordException{
 		
 		// first figure out the symmetric key
@@ -85,5 +97,44 @@ public class EncryptedMessage{
 		// now use symmetric key to decrypte
 		SymmetricEncryptor symmetric = new SymmetricEncryptor(secretKey);
 		return symmetric.decryptByteArrayToString(encryptedMessage);
+	}
+	
+	/**
+	 * Decrypt the message with the senders private key
+	 * @param sendersPrivateKey
+	 * @return the decrypted message
+	 * @throws EncryptionException 
+	 * @throws IncorrectPasswordException 
+	 * @throws IOException 
+	 */
+	public String decryptMessageForSender(PrivateKey sendersPrivateKey)
+			throws EncryptionException, IOException, IncorrectPasswordException{
+		
+		// first figure out the symmetric key
+		PublicPrivateEncryptor receiver = new PublicPrivateEncryptor(sendersPrivateKey, null);
+		byte[] secretKey = receiver.decryptWithPrivate(encryptedSecretKeyForSender);
+		
+		// now use symmetric key to decrypte
+		SymmetricEncryptor symmetric = new SymmetricEncryptor(secretKey);
+		return symmetric.decryptByteArrayToString(encryptedMessage);
+	}
+	
+	/**
+	 * Decrypt the message with either the senders or receivers private key. Will figure out which to use
+	 * @param keyPair A pair of public and private keys
+	 * @return The decrypted message
+	 * @throws EncryptionException
+	 * @throws IOException
+	 * @throws IncorrectPasswordException
+	 */
+	public String decryptMessage(PublicPrivateEncryptor keyPair)
+			throws EncryptionException, IOException, IncorrectPasswordException{
+		// first determine if sender or receiver
+		if(sendersPublicKey.equals(keyPair.getPublic()))
+			return decryptMessageForSender(keyPair.getPrivate());
+		else if (receiversPublicKey.equals(keyPair.getPublic()))
+			return decryptMessageForReceiver(keyPair.getPrivate());
+		else
+			throw new EncryptionException("No Matching Private Key");
 	}
 }
